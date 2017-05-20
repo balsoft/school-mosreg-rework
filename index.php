a11642a606b76aef4d4767159fa1b8e95ee90fff
Alexander Bantyev <balsoft@yandex.ru>
2017-05-20 16:06:09 +0300

    Header

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <title>Школьный портал для людей</title>
     </head>
     <body>
-        <div class="mdl-layout mdl-js-layout">
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
             <header class="mdl-layout__header">
                 <div class="mdl-layout__header-row">
                 <!-- Title -->
