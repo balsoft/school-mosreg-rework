3205d6278d534a3e4d8ecae0fad540dbe4c9836d
Alexander Bantyev <balsoft@yandex.ru>
2017-05-20 15:56:57 +0300

    container

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        <title>Школьный портал для людей</title>
     </head>
-    <body>
+    <body class="container">
         <?php
             print "PHP работает";
         ?>
-        <button class='mdl-button mdl-button--raised mdl-js-button mdl-button--accent' onclick='alert("Молодец")'>нажми меня</button>
+        <button class='mdl-button mdl-button--raised mdl-js-button mdl-button--accent mdl-js-ripple-effect' onclick='alert("Молодец")'>нажми меня</button>
     </body>
 </html>
